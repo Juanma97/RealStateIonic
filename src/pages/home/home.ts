295a0c94c291ceb90c6704b8995e2c8fe82635a8
Juan Manuel Perez Toro <juanma97@outlook.com>
2018-11-04 20:42:10 +0000

    show data from firebase

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import * as firebase from 'firebase/app';
+import { AngularFireObject, AngularFireList} from 'angularfire2/database';
+import { Observable } from 'rxjs';
+import { AngularFireDatabase } from '@angular/fire/database';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  viviendas: Observable<any[]>;
+  
 
+  constructor(db2: AngularFireDatabase) {
+    const listRef = db2.list('viviendas');
+    this.viviendas = db2.list('viviendas').valueChanges();
+    console.log(this.viviendas);
   }
 
 }
