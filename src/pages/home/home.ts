0fc264deef9a39ec6330418fdea8df926df06e3c
Juan Manuel Perez Toro <juanma97@outlook.com>
2018-11-27 22:42:38 +0000

    fg     h

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,8 +1,10 @@
+import { PropertyDetailsPage } from './../property-details/property-details';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import * as firebase from 'firebase/app';
 import { AngularFireObject, AngularFireList} from 'angularfire2/database';
 import { Observable } from 'rxjs';
+import { filter } from 'rxjs/operators';
 import { AngularFireDatabase } from '@angular/fire/database';
 @Component({
   selector: 'page-home',
@@ -10,13 +12,15 @@ import { AngularFireDatabase } from '@angular/fire/database';
 })
 export class HomePage {
 
+  propertyDetails ="PropertyDetailsPage";
   viviendas: Observable<any[]>;
   
 
-  constructor(db2: AngularFireDatabase) {
+  constructor(public navCtrl: NavController, db2: AngularFireDatabase) {
     const listRef = db2.list('viviendas');
     this.viviendas = db2.list('viviendas').valueChanges();
     console.log(this.viviendas);
   }
+
 
 }
